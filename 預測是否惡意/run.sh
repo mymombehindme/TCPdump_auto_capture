799f603b5382089d4d57428a4c4b7e3d7cf2f4b5
mymombehindme <47748913+mymombehindme@users.noreply.github.com>
2021-11-26 22:13:56 +0800

    Update run.sh
    修改原因同上一個資料夾

--- 預測是否惡意/run.sh
+++ 預測是否惡意/run.sh
@@ -1,22 +1,23 @@
 #! /bin/bash
-rm -f `find csv -type f` 2> /dev/null
+rm -f `find csv -type f`2> /dev/null
 while true
  do
     sleep 2
     touch file.txt
-    echo "create file\n"
     echo -n "./csv/" > file.txt
-    ls ./csv >> file.txt 
+    ls ./csv >> file.txt
     name=`cat file.txt`
     if [[ "$name" == "./csv/" ]];
     then
+	echo "No csv file in csv folder"
         rm file.txt
         continue
     fi
+    echo "Start prediction!"
     cat file.txt | python3 auto_test_label.py
     echo  -n  "file3.csv" > file.txt
     cat file.txt | python3 xgbmodel_predict.py
     rm file3.csv 2> /dev/null
     rm file.txt
-    rm -f `find csv -type f` 2> /dev/null
+    rm -f `find ./csv -type f` 2> /dev/null
 done
